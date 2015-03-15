9a9cab18200ae852bd722cb547501662fffe3ef0
Vlad Ungureanu <vvu@vdev.ro>
2015-03-15 22:42:16 +0100

    New tools.
    Signed-off-by: Vlad Ungureanu <vvu@vdev.ro>

--- lmapd-collector.py
+++ lmapd-collector.py
@@ -2,12 +2,15 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
 @app.route('/data', methods=['POST'])
 def upload_data():
+    print request.values
     return jsonify({'result': 'OK'}), 200
 
 if __name__ == '__main__':
