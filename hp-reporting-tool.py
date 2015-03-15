7d78b3e5056a0694067deed932f4f77047767a59
Vlad Ungureanu <vvu@vdev.ro>
2015-03-15 23:21:25 +0100

    Removing file after send.
    Signed-off-by: Vlad Ungureanu <vvu@vdev.ro>

--- hp-reporting-tool.py
+++ hp-reporting-tool.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import os
 
 __author__ = 'vvu'
 
@@ -12,3 +13,6 @@ with open(data, 'rb') as payload:
     headers = {'content-type': 'application/x-www-form-urlencoded'}
     r = requests.post(post_url,
                       data=payload, verify=False, headers=headers)
+
+if os.path.isfile(data):
+    os.unlink(data)
