40ff429104e0716244895f688d1589468664ff5e
Vlad Ungureanu <vvu@vdev.ro>
2015-03-15 17:29:41 +0100

    First commit.
    Signed-off-by: Vlad Ungureanu <vvu@vdev.ro>

---
+++ lmapd-collector.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/data', methods=['POST'])
+def upload_data():
+    return jsonify({'result': 'OK'}), 200
+
+if __name__ == '__main__':
+    app.run()
