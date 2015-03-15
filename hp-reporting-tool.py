ada158d3f6778b8ffb211cb1f51a077451b83b74
Vlad Ungureanu <vvu@vdev.ro>
2015-03-15 22:42:41 +0100

    Reporting tool.
    Signed-off-by: Vlad Ungureanu <vvu@vdev.ro>

--- hp-reporting-tool.py
+++ hp-reporting-tool.py
@@ -1,1 +1,14 @@
+#!/usr/bin/python
+
 __author__ = 'vvu'
+
+import sys
+import requests
+
+post_url = sys.argv[1]
+data = sys.argv[2]
+
+with open(data, 'rb') as payload:
+    headers = {'content-type': 'application/x-www-form-urlencoded'}
+    r = requests.post(post_url,
+                      data=payload, verify=False, headers=headers)
