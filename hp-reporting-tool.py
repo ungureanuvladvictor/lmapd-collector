188e15f851bf5c62fae4711e3286db6206548c71
Vlad Ungureanu <vvu@vdev.ro>
2015-03-25 19:08:51 +0100

    Updated reporting tool.
    Signed-off-by: Vlad Ungureanu <vvu@vdev.ro>

--- hp-reporting-tool.py
+++ hp-reporting-tool.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import os
+import glob
 
 __author__ = 'vvu'
 
@@ -9,10 +10,11 @@ import requests
 post_url = sys.argv[1]
 data = sys.argv[2]
 
-with open(data, 'rb') as payload:
-    headers = {'content-type': 'application/x-www-form-urlencoded'}
-    r = requests.post(post_url,
-                      data=payload, verify=False, headers=headers)
-
-if os.path.isfile(data):
-    os.unlink(data)
+for filename in os.listdir(data):
+	with open(data+"/"+filename, 'rb') as payload:
+		headers = {'content-type': 'application/x-www-form-urlencoded'}
+		r = requests.post(post_url,
+			data=payload, verify=False, headers=headers)
+files = glob.glob(data+"/*")
+for f in files:
+	os.remove(f)
