b35b763d6c4f3fc749fb3c99265810270b6dc36c
Vlad Ungureanu <vvu@vdev.ro>
2015-04-29 14:02:36 +0200

    Added timeout.
    Signed-off-by: Vlad Ungureanu <vvu@vdev.ro>

--- hp-reporting-tool.py
+++ hp-reporting-tool.py
@@ -14,7 +14,7 @@ for filename in os.listdir(data):
 	with open(data+"/"+filename, 'rb') as payload:
 		headers = {'content-type': 'application/x-www-form-urlencoded'}
 		r = requests.post(post_url,
-			data=payload, verify=False, headers=headers)
+			data=payload, verify=False, headers=headers, timeout=0.1)
 files = glob.glob(data+"/*")
 for f in files:
 	os.remove(f)
